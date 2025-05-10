504d7a7b369168ba8d15728fb1c054c10f0a83aa
AndrePimenta2005 <95694734+AndrePimenta2005@users.noreply.github.com>
2025-05-10 12:37:36 +0100

    rename to iteradorBD.lua and progress on extratorAE2.lua

--- extratorAE2.lua
+++ extratorAE2.lua
@@ -1,1 +1,13 @@
+local component = require("component")
+local interface = component.proxy(component.list("me_interface"), true)
+local db = component.proxy(component.list("database"), true)
+local iteradorBd = require("iteradorBD")
 
+::loop::
+
+--local lista = interface.getItemsInNetwork()
+if #(interface.getItemsInNetwork())>0 then
+    for key, value in ipairs(interface.getItemsInNetwork()) do
+    
+    end
+end
