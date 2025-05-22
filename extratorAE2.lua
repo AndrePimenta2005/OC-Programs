18ca9d709f3cb112691d7b05b6f57570915e8863
AndrePimenta2005 <95694734+AndrePimenta2005@users.noreply.github.com>
2025-05-22 16:12:43 +0100

    solving a possible scope issue

--- extratorAE2.lua
+++ extratorAE2.lua
@@ -4,14 +4,15 @@ local db = component.proxy(component.list("database"), true)
 local iteradorBd = require("iteradorBD")
 
 ::loop::
+do
+    --local lista = interface.getItemsInNetwork()
 
---local lista = interface.getItemsInNetwork()
+    for i=1, 9 do
+        local tempItem = interface.getItemsInNetwork()[i]
+        interface.store(tempItem, db.address, i)
+        interface.setInterfaceConfiguration(i, database.address, i, tempItem.maxSize)
+        db.clear(i)
+    end
 
-for i=1, 9 do
-    local tempItem = interface.getItemsInNetwork()[i]
-    interface.store(tempItem, db.address, i)
-    interface.setInterfaceConfiguration(i, database.address, i, tempItem.maxSize)
-    db.clear(i)
 end
-
 goto loop;
