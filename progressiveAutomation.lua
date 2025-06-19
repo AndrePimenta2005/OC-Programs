bd5bcd949822c3c017b0274c9b04d783e0c7f931
AndrePimenta2005 <95694734+AndrePimenta2005@users.noreply.github.com>
2025-06-19 12:29:01 +0100

    progressiveAutomation.lua done

--- progressiveAutomation.lua
+++ progressiveAutomation.lua
@@ -1,4 +1,33 @@
-local component = require("component")
+local transposer = require("component").transposer
 local sides = require("sides")
 local sideWheat = sides.east
 local sideSeed = sides.up
+local sidePlanter = sides.north
+local sleep = require("os").sleep
+local string = require("string")
+
+::redo::
+do
+    local seedsReserve = 0
+    for i = 5, 13 do
+        local item = transposer.getStackInSlot(sidePlanter, i)
+        if item then
+            local idItem = item.id:match(":(%a+)")
+            if idItem == "wheat" then
+                transposer.transferItem(sidePlanter, sideWheat, item.qty, i, 1)
+            elseif idItem == "wheat_seeds"
+                if seedsReserve == 0 then
+                    seedsReserve = 1
+                else
+                    transposer.transferItem(sidePlanter, sideSeed, item.qty, i, 1)
+                end
+            end
+        end
+    end
+    local seedsPlant = transposer.getSlotStackSize(sidePlanter, 2)
+    if (seedsPlant == 0) or (seedsReserve == 0) then
+        transposer.transferItem(sideSeed, sidePlanter, 1, 2, 2)
+    end
+end
+sleep(0)
+goto redo
